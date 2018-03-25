6501f9602aefc5705d7312f1a1515a4f4ca8bcdf
Tom Meagher <thom.meagher@gmail.com>
2018-03-24 20:12:47 -0400

    Initial Commit

---
+++ vodka-soda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vodka-soda
+//
+//  Created by Tom Meagher on 3/24/18.
+//  Copyright © 2018 Tom Meagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
